c47bc841a829dbb06b44b0353ed59391bd7cb980
demidovahelenka00-ctrl <demidovahelenka00@gmail.com>
2026-05-04 15:38:22 +0000

    Update Firebase configuration with new credentials

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyDOdsZx4Es1sWSvEr-Nm7sv6l6GPJuRKDw",
-  authDomain: "mtn-rewards-d4faa.firebaseapp.com",
-  projectId: "mtn-rewards-d4faa",
-  storageBucket: "mtn-rewards-d4faa.firebasestorage.app",
-  messagingSenderId: "566636707957",
-  appId: "1:566636707957:web:a0b066ac5a498c01d5b83c",
-  measurementId: "G-R13RFCYKDY"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAcqpVF5ZPRC97i83Ss531OxORR-vGO638",
+    authDomain: "mtn-rewards-d6c6f.firebaseapp.com",
+    projectId: "mtn-rewards-d6c6f",
+    storageBucket: "mtn-rewards-d6c6f.firebasestorage.app",
+    messagingSenderId: "736865464673",
+    appId: "1:736865464673:web:8bdc11af0226912f065520",
+    measurementId: "G-8N0ZZTP93F"
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
