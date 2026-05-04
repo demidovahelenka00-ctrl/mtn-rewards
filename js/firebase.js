1f36276418105be7c591d51d223753d0380129ea
demidovahelenka00-ctrl <demidovahelenka00@gmail.com>
2026-05-04 14:46:42 +0000

    Update Firebase imports and configuration format

--- js/firebase.js
+++ js/firebase.js
@@ -10,28 +10,28 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-npm install firebase
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA_X7SMHYfht72e-iblOdODCuyFMi1tnu4",
-  authDomain: "mtn-rewards.firebaseapp.com",
-  projectId: "mtn-rewards",
-  storageBucket: "mtn-rewards.firebasestorage.app",
-  messagingSenderId: "868026445194",
-  appId: "1:868026445194:web:52f9587b159a050eb3ec73",
-  measurementId: "G-KL0SRY5TW1"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA_X7SMHYfht72e-iblOdODCuyFMi1tnu4",
+    authDomain: "mtn-rewards.firebaseapp.com",
+    projectId: "mtn-rewards",
+    storageBucket: "mtn-rewards.firebasestorage.app",
+    messagingSenderId: "868026445194",
+    appId: "1:868026445194:web:52f9587b159a050eb3ec73",
+    measurementId: "G-KL0SRY5TW1"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
