75d66670d6006897c13b90dba6265eaf6dc1f57d
demidovahelenka00-ctrl <demidovahelenka00@gmail.com>
2026-05-04 15:32:39 +0000

    Update Firebase configuration and imports

--- js/firebase.js
+++ js/firebase.js
@@ -1,37 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
 const firebaseConfig = {
-  apiKey: "PASTE HERE",
-  authDomain: "PASTE HERE",
-  projectId: "PASTE HERE",
-  appId: "PASTE HERE"
+  apiKey: "AIzaSyDOdsZx4Es1sWSvEr-Nm7sv6l6GPJuRKDw",
+  authDomain: "mtn-rewards-d4faa.firebaseapp.com",
+  projectId: "mtn-rewards-d4faa",
+  storageBucket: "mtn-rewards-d4faa.firebasestorage.app",
+  messagingSenderId: "566636707957",
+  appId: "1:566636707957:web:a0b066ac5a498c01d5b83c",
+  measurementId: "G-R13RFCYKDY"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA_X7SMHYfht72e-iblOdODCuyFMi1tnu4",
-    authDomain: "mtn-rewards.firebaseapp.com",
-    projectId: "mtn-rewards",
-    storageBucket: "mtn-rewards.firebasestorage.app",
-    messagingSenderId: "868026445194",
-    appId: "1:868026445194:web:52f9587b159a050eb3ec73",
-    measurementId: "G-KL0SRY5TW1"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
